f559e7220d6c916e0a7be26bb28b4a7245baa188
tianyuty0104 <365189618@qq.com>
2020-04-08 07:37:02 +0800

    更新

--- src/main/resources/static/js/pages/dtgj/bmfw/yswp/yswplook.js
+++ src/main/resources/static/js/pages/dtgj/bmfw/yswp/yswplook.js
@@ -13,6 +13,7 @@ layui.config({
     var fFileListView = $('#fileList');//文件列表
     //获取文件列表
     function getFileData() {
+        $("#div_wpzp").hide();
         var index = layer.load(1);
         $.ajax({
             type: 'get',
@@ -24,10 +25,13 @@ layui.config({
                 layer.close(index);
                 if (data.code == "1") {
                     var items = data.data;
-                    fFileListView.find('tr.get-list').remove();
-                    for (var i = 0; i < items.length; i++) {
-                        var item = items[i];
-                        trAddData(item);
+                    if(items.length>0) {
+                        $("#div_wpzp").show();
+                        fFileListView.find('tr.get-list').remove();
+                        for (var i = 0; i < items.length; i++) {
+                            var item = items[i];
+                            trAddData(item);
+                        }
                     }
                 } else {
                     msg('数据加载失败，请重试', {
